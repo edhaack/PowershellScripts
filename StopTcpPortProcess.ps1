d7783fe30ec2620d1241a55a7353aedada96d2e6
edhaack <email@edhaack.net>
2016-02-26 11:57:13 -0600

    Update StopTcpPortProcess.ps1

--- StopTcpPortProcess.ps1
+++ StopTcpPortProcess.ps1
@@ -1,14 +1,18 @@
-$rawResult = netstat -ano | Select-String "2002"
+$rawResult = netstat -ano | Select-String "8888"
 #{  
 #TCP    0.0.0.0:2002           0.0.0.0:0              LISTENING       9816,   
 #TCP    127.0.0.1:2002         127.0.0.1:55256        ESTABLISHED     9816,   
 #TCP    127.0.0.1:55256        127.0.0.1:2002         ESTABLISHED     8108}
-$firstResult = $rawResult[0]
-#  TCP    0.0.0.0:2002           0.0.0.0:0              LISTENING       9816
-$finalResult = $firstResult -replace " "
-#TCP0.0.0.0:20020.0.0.0:0LISTENING9816
-$listening = "LISTENING"
-$listeningIndex = $finalResult.IndexOf("G") + 1
-$tcpPortToKill = $finalResult.Substring($listeningIndex)
-#9816
-Stop-Process $tcpPortToKill
+if ($rawResult) { 
+	$firstResult = $rawResult[0]
+	#  TCP    0.0.0.0:2002           0.0.0.0:0              LISTENING       9816
+	$finalResult = $firstResult -replace " "
+	#TCP0.0.0.0:20020.0.0.0:0LISTENING9816
+	$listening = "LISTENING"
+	$listeningIndex = $finalResult.IndexOf("G") + 1
+	$tcpPortToKill = $finalResult.Substring($listeningIndex)
+	#9816
+	Stop-Process $tcpPortToKill
+} else {
+	"Nothing to kill..."
+}
